1d6c9f1d58936a74697c2db0042b2079b5ff1bf0
Xinyuan Zhang <xiz016@ucsd.edu>
2016-02-17 00:41:22 +0000

    analytics lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,9 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+	$(".likeBtn").click(likeClick);
+}
+
+function likeClick(e) {
+	ga("send", "event", "like", "click");
 }
