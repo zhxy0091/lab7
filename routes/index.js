ae1618fe50d7234b5a8ef0659d47eae87867de0d
Xinyuan Zhang <xiz016@ucsd.edu>
2016-02-17 01:01:19 +0000

    added coin flip

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,16 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(req, res){
-	projects["grid"] = false;
-  	res.render('index', projects);
+	var random_num = Math.random();
+	console.log(random_num);
+	if(random_num > 0.5) {
+		projects["grid"] = false;
+  		res.render('index', projects);
+	}
+	else {
+		res.redirect('/grid');
+	}
+	
 };
 
 exports.viewGrid = function(req, res){
